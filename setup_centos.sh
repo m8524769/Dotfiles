02490331312f4344b29acb89827326f16b30d111
m8524769 <m8524769@163.com>
2018-10-14 12:46:50 +0800

    Add nginx and ssr

--- setup_centos.sh
+++ setup_centos.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-yum update
+OS=`cat /etc/os-release | grep "ID" | awk -F "[\"\"]" '{print $2}' | head -n1`
+OSRELASE=`cat /etc/os-release | grep "VERSION_ID" | awk -F "[\"\"]" '{print $2}'`
+
+if [[ $OS != "centos" ]] ; then
+    echo "Sorry, your operate system is not CentOS."
+    exit 1
+fi
+
+yum update -y
 
 yum install -y git vim zsh
 
 chsh -s /bin/zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 curl -fLo ~/.zshrc \
     https://raw.githubusercontent.com/m8524769/Dotfiles/master/.zshrc
@@ -22,4 +28,31 @@ curl -fLo ~/.vim/colors/molokai2.vim --create-dirs \
 
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions \
     ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+read -p "Install & Setup Nginx? (Y/n)" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    nginxRepo="/etc/yum.repos.d/nginx.repo"
+    echo "[nginx]" > $nginxRepo
+    echo "name=nginx repo" >> $nginxRepo
+    echo "baseurl=http://nginx.org/packages/$OS/$OSRELEASE/$basearch/" >> $nginxRepo
+    echo "gpgcheck=0" >> $nginxRepo
+    echo "enabled=1" >> $nginxRepo
+    yum install -y nginx
+    curl -fLo /etc/nginx/nginx.conf \
+        https://raw.githubusercontent.com/m8524769/Dotfiles/master/nginx/nginx.conf
+    iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+    systemctl enable nginx
+fi
+
+read -p "Setup ShadowSocksR? (Y/n)" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
+    chmod +x shadowsocksR.sh
+    ./shadowsocksR.sh 2>&1 | tee ShadowSocksR.log
+    wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+    chmod +x bbr.sh
+    ./bbr.sh
+fi
 
