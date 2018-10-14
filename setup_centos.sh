be3ac6faa806e0c076eebe8a9f144099e8b94765
m8524769 <m8524769@163.com>
2018-10-14 10:00:41 +0800

    Update setup_centos.sh

--- setup_centos.sh
+++ setup_centos.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+yum update
+
 yum install -y git vim zsh
 
 chsh -s /bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions \
-    ~/.oh-my-zsh/plugins/zsh-autosuggestions
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.zshrc \
+    https://raw.githubusercontent.com/m8524769/Dotfiles/master/.zshrc
 
 curl -fLo ~/.vimrc \
     https://raw.githubusercontent.com/m8524769/Dotfiles/master/.vimrc_for_server
 
-curl -fLo /usr/share/vim/vimfiles/colors/molokai2.vim \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.vim/colors/molokai2.vim --create-dirs \
     https://raw.githubusercontent.com/m8524769/Dotfiles/master/others/molokai2.vim
+
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions \
+    ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
